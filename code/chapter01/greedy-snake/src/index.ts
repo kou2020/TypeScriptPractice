601dcac7e809f7c2a14d8dbe0166d356c2376342
kou2020 <shangguangchunxue@gmail.com>
2024-10-03 00:14:29 +0900

    Revert "TypeScriptPractice初次提交"
    This reverts commit 685a2b117d31ecc5f026cc92e6703d0455ff20a0.

--- code/chapter01/greedy-snake/src/index.ts
+++
@@ -1,7 +0,0 @@
-import "./style/index.less";
-import GameControl from "./modules/GameControl";
-
-const gameControl = new GameControl();
-// setInterval(() => {
-//   console.log(gameControl.direction);
-// }, 1000);
--- code/chapter01/part2/src/app.ts
+++
@@ -1,5 +0,0 @@
-import { hi } from "./m";
-console.log("app");
-let a = 10;
-let b = 33;
-console.log(hi);
